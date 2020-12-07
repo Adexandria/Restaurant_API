a5187fdec65437388c765c78e2886dd952261a12
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-12-07 11:32:24 +0100

    Added project and included owin packages

---
+++ DeResturant_API/Services/OwinDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeResturant_API.Services
+{
+    public class OwinDbContext :IdentityDbContext
+    {
+        public OwinDbContext() : base("Identity")
+        {
+
+        }
+    }
+}
